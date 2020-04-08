98a26227c75bf462fdfe4d668075d115a795a24b
Prachit99 <prachitmhalgi@gmail.com>
2020-04-08 20:44:03 +0530

    winning condition added

--- sketch.js
+++ sketch.js
@@ -10,13 +10,35 @@ let curr_player;
 
 function setup() {
 	createCanvas(400, 400);
-	
+	frameRate(2);
 	curr_player = floor(random(players.length)); 
 	for (let i = 0; i < 3; i++) {
 		for (let j = 0; j < 3; j++) {
 			available.push([i,j]);
 		}
 	}	
+}
+
+function equal(a, b, c) {
+	if (a == b && b == c && a != '')	return true;
+	else 	return false; 
+}
+
+function check() {
+	let winner =  null;
+	for (let i = 0; i < 3; i++) {
+		if (equal(board[i][0], board[i][1], board[i][2]))	winner = board[i][0];
+	}
+	for (let i = 0; i < 3; i++) {
+		if (equal(board[0][i], board[1][i], board[2][i]))	winner = board[0][i];
+	}
+
+	if (equal(board[0][0], board[1][1], board[2][2])) 	winner = board[0][0];
+	else if (equal(board[0][2], board[1][1], board[2][0]))	winner = board[0][2];
+
+	if (winner == null && available.length == 0) return 'Tie';
+	else 	return winner;
+
 }
 
 function nextTurn() {
@@ -59,5 +81,15 @@ function draw() {
 		}
 	}
 
-	nextTurn();
+
+	let res = check();
+	if (res != null) {
+		noLoop();
+		if (res == 'Tie')	createP("Result: " + res).style('color', '#000').style('font-size', '32pt');
+		else 			createP(res + " is the winner!").style('color', '#000').style('font-size', '32pt');
+	}
+	else {
+		nextTurn();
+	}
+
 }
